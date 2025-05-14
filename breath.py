08d8d81701be29e182b56069795beab783dafdae
regular bill <regularbill@bill.com>
2025-05-14 14:27:09 -0500

    052514142709

--- breath.py
+++ breath.py
@@ -3,7 +3,7 @@ import argparse
 import os
 
 def play_sound(sound_file):
-    """Plays the sound file using mplayer."""
+    """Plays the sound file using mpg123 with quiet output."""
     os.system(f"mpg123 -q {sound_file} &")
 
 def breathing_coach(inhale, hold, exhale, rounds):
@@ -11,11 +11,12 @@ def breathing_coach(inhale, hold, exhale, rounds):
     round_count = 0
     if rounds is None or rounds <= 0:
         while True:
+            os.system('clear')  # Clear the screen
             round_count += 1
-            print(f"\nRound: {round_count}")
+            print(f"Round: {round_count}")
 
             print("Inhale...", end='\r')
-            play_sound("gong-inhale.mp3") 
+            play_sound("gong-inhale.mp3")
             for i in range(int(inhale), 0, -1):
                 print(f"Inhale... {i}", end='\r')
                 time.sleep(1)
@@ -37,7 +38,8 @@ def breathing_coach(inhale, hold, exhale, rounds):
             print("Exhale... Done")
     else:
         for round_count in range(1, int(rounds) + 1):
-            print(f"\nRound: {round_count}")
+            os.system('clear')  # Clear the screen
+            print(f"Round: {round_count}")
 
             print("Inhale...", end='\r')
             play_sound("gong-inhale.mp3")
